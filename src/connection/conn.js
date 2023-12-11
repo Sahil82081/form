7b33e2a9dbcedfb3b303451762d55fd3cd2f4239
Sahil <sahilkumbharkar57@gmail.com>
2023-12-11 10:48:27 +0530

    S

--- src/connection/conn.js
+++ src/connection/conn.js
@@ -1,8 +1,18 @@
+// const mongoose = require("mongoose")
+// const connect = mongoose.connect("mongodb://localhost:27017")
+
+// connect.then(() => {
+//     console.log("Connected To a Database")
+// }).catch((e) => {
+//     console("Conncction Error" + e)
+// })
+
 const mongoose = require("mongoose")
-const connect = mongoose.connect("mongodb://localhost:27017")
+
+const connect = mongoose.connect("mongodb+srv://sahilkumbharkar:FkHW1KkzxiNewNPH@sahil.wc1irsj.mongodb.net/?retryWrites=true&w=majority")
 
 connect.then(() => {
     console.log("Connected To a Database")
 }).catch((e) => {
-    console("Conncction Error" + e)
+    console.log("Conncction Error : " + e)
 })
